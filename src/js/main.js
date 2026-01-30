dbf63298464dfc4954d8ab0615ee7a5ed15fd15f
Hanna Lindkvist <hali2507@student.miun.se>
2026-01-30 15:20:39 +0100

    skriver funktion för sök filtrering + ändrar hämtning av API

--- src/js/main.js
+++ src/js/main.js
@@ -2,24 +2,35 @@
 
 //ladda DOM
 document.addEventListener("DOMContentLoaded", () => {
-    fetchData("https://webbutveckling.miun.se/files/ramschema.json");
+    readData();
 });
 
 //hämta JSON-data
-async function fetchData(url) {
+async function fetchData() {
     try {
-        const response = await fetch(url);
+        const response = await fetch("https://webbutveckling.miun.se/files/ramschema.json");
         const data = await response.json();
-        writeTable(data);
+        return data;
     } catch (error) {
         console.error(`Felmeddelande ${error}`);
     }
 }
 
+async function readData() {
+    const data = await fetchData();
+
+    writeTable(data);
+
+    document.querySelector("#searchInput").addEventListener("input", () => {
+        searchFilter(data);
+    });
+}
+
 function writeTable(tableData) {
     const tableEl = document.querySelector("#tableData");
+    tableEl.innerHTML = "";
 
-    tableData.forEach(data => {
+    tableData.forEach(d => {
         //skapa tr + td element
         const trEl = document.createElement("tr");
         const tdCodeEl = document.createElement("td");
@@ -27,9 +38,9 @@ function writeTable(tableData) {
         const tdProgEl = document.createElement("td");
 
         //lägg till text
-        tdCodeEl.innerHTML = data.code;
-        tdNameEl.innerHTML = data.coursename;
-        tdProgEl.innerHTML = data.progression;
+        tdCodeEl.innerHTML = d.code;
+        tdNameEl.innerHTML = d.coursename;
+        tdProgEl.innerHTML = d.progression;
 
         //lägger till td i tr
         trEl.appendChild(tdCodeEl);
@@ -39,4 +50,14 @@ function writeTable(tableData) {
         //skriv ut till DOM
         tableEl.appendChild(trEl);
     });
+}
+
+function searchFilter(tableArr) {
+    let input = document.querySelector("#searchInput").value.toLowerCase();
+    let tableArrFilterd = tableArr.filter((table) =>
+        table.code.toLowerCase().includes(input) ||
+        table.coursename.toLowerCase().includes(input)
+    );
+
+    writeTable(tableArrFilterd);
 }
